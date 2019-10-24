6bc17500a172b2adbd07831cc9194a863db5879e
Carlos Abreu <ocariocawebdesign@gmail.com>
2019-10-24 17:33:59 -0300

    Alteração no gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     plugins: [
 
-        `gatsby-plugin-styled-components`,
+        
 
         `gatsby-plugin-react-helmet`,
         {
@@ -30,6 +30,7 @@ module.exports = {
                 //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
         },
+        `gatsby-plugin-styled-components`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
