a696dcb4968ac87f0329c54098abdf921eb2204c
Carlos Abreu <ocariocawebdesign@gmail.com>
2019-10-24 21:56:59 -0300

    correção

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,31 +1,25 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Logo = () => {
-
-    const { logoImage } = useStaticQuery(
-        graphql `
-            query {  
-                
-                logoImage: file(relativePath:{ eq:"logo.png" }) {
-                    childImageSharp{    
-                        fluid(maxWidth: 120, maxHeight: 120) {
-                            ...GatsbyImageSharpFluid
-                        }
-                    }
-                }
+  const { logoImage } = useStaticQuery(
+    graphql`
+      query {
+        logoImage: file(relativePath: { eq: "logo.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 120, maxHeight: 120) {
+              ...GatsbyImageSharpFluid
             }
-        
-        `
-    )
+          }
+        }
+      }
+    `
+  )
 
-    return <Img fluid = { logoImage.childImageSharp.fluid }
-    style = {
-        { width: "120px" } }
-    />
+  return (
+    <Img fluid={logoImage.childImageSharp.fluid} style={{ width: "120px" }} />
+  )
 }
 
-
-
-export default Logo;
+export default Logo
